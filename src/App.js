b24455aa88ccba145dce1173228d10e2df3a1582
Stephano <stephan.orgiazzi@gmail.com>
2022-07-04 12:49:44 +0200

    added lazyloading

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
+import React, { Suspense } from 'react'
+
 import { Route, Routes } from 'react-router-dom';
 
 import AllMovies from './components/AllMovies/AllMovies'
-import MoviePage from './components/MoviePage/MoviePage'
-import Layout from './components/Layout/Layout';
-import BestMovies from './components/BestMovies/BestMovies';
+import Layout from './components/Layout/Layout'
+import BestMovies from './components/BestMovies/BestMovies'
+
+const MoviePage = React.lazy(() => import('./components/MoviePage/MoviePage'))
 
 const App = () => {
     return <Layout>
-        <Routes>
-            <Route path='/' element={<><BestMovies /><AllMovies /></>} />
-            <Route path='/movie' element={<MoviePage />} />
-            <Route path='/best' element={<BestMovies />} />
-            <Route path='/movie/:movieId' element={<MoviePage />} />
-            <Route path='*' element={<div>yoyoyoy</div>} />
-        </Routes>
+        <Suspense fallback={<p>Loading...</p>}>
+            <Routes>
+                <Route path='/' element={<><BestMovies /><AllMovies /></>} />
+                <Route path='/movie' element={<MoviePage />} />
+                <Route path='/best' element={<BestMovies />} />
+                <Route path='/movie/:movieId' element={<MoviePage />} />
+                <Route path='*' element={<div>yoyoyoy</div>} />
+            </Routes>
+        </Suspense>
     </Layout>
 }
 
