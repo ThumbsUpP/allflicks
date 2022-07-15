b4bf02cd4cc24c7541224719c2b11d10f4dd632e
Pierre CAYOL <pierre.cayol@gmail.com>
2022-07-15 14:42:57 +0200

    Update MoviesList.js
    cette div '<div className={styles['movie-card']} key={movie.id}>' devrait être dans MovieCard non ?

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -2,21 +2,19 @@ import MovieCard from '../MovieCard/MovieCard'
 
 import styles from './MoviesList.module.scss'
 
-const MoviesList = props => {
-    const movies = props.movies.map(movie => (
-        <div className={styles['movie-card']} key={movie.id}>
-            <MovieCard
-                id={movie.id}
-                title={movie.title}
-                cover={movie.poster_path}
-                year={(movie.release_date || '').substring(0, 4)}
-            />
-        </div>
-    ))
-
-    return <ul className={styles.list}>
-        {movies}
+const MoviesList = ({movie}) =>  
+    <ul className={styles.list}>
+        {
+            movies.map(movie => (
+            <div className={styles['movie-card']} key={movie.id}>
+                <MovieCard
+                    id={movie.id}
+                    title={movie.title}
+                    cover={movie.poster_path}
+                    year={(movie.release_date || '').substring(0, 4)}
+                />
+            </div>
+          ))}
     </ul>
-}
 
 export default MoviesList
